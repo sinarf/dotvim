15b30a1a2ffe22c0beedf48e692214efaabebdd9
Michel Blavin <michel@blavin.fr>
2012-07-27 15:58:06 +0200

    Fucntinnal script to copie the .vimrc and .gvimrc file to the home directory. Note : the copied file are set read only for obvious reasons.

--- PutVimrcFilesInHome.groovy
+++ PutVimrcFilesInHome.groovy
@@ -1,17 +1,41 @@
-!groovy
+#!groovy
 
 /**
  *  Copy the rc file to home.
  *
  * @author Michel Blavin 
  */
+
+// TODO do the symbolic links for linux. 
+
 def ENV = System.getenv()
 def HOME = ENV['HOME']
+
+boolean accept = false // the user hasn't accepted the overide yet. 
+
 if (!HOME) throw new IllegalStateException ("HOME variable not set. Please, do.")
 
+System.in.withReader {
+	print 'Would you overwrite the .vimrc and .gvimrc in your HOME directory. : '
+	String response = it.readLine().toUpperCase()
+	accept = response &&  response.startsWith("Y")
 
-println 'Copie files'
-new File("$HOME/.vimrc") << new File(".vimrc").asWritable()
-new File("$HOME/.gvimrc") << new File(".gvimrc").asWritable()
+}
 
-println 'done'
+if (accept) {
+	println 'Copie files'
+	def vimrc = new File("$HOME/.vimrc")
+	vimrc.writable = true
+	vimrc.write ''
+	vimrc << new File(".vimrc").asWritable()
+	vimrc.writable = false
+
+	def gvimrc = new File("$HOME/.gvimrc") 
+	gvimrc.writable = true
+	gvimrc.write ''
+	gvimrc << new File(".gvimrc").asWritable()
+	gvimrc.writable = false
+	println 'done'
+} else {
+	println 'Nothing has been done!'
+}
