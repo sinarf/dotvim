bae3352211361c80e76c3351bfee4b6f966ba18a
Michel Blavin - VIF <michel.blavin@vif.fr>
2013-01-17 18:04:47 +0100

    vim configuration reboot !!!:!

--- git_module.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python 
-
-# this is meant to automate adding or removing a git module, since there is no simple way to remove a module from git. 
-import dulwich 
-
-import mkdir from os 
-
